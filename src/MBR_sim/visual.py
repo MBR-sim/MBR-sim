57f7f0d090c9ce7dd9c531db5b548345e3c8d862
MEHUL GOEL <mehulgoel873@gmail.com>
2022-10-06 22:09:44 -0700

    Added cycles to resource visualization

--- src/MBR_sim/visual.py
+++ src/MBR_sim/visual.py
@@ -12,25 +12,28 @@ def resource_table(hw_cfg, graph):
     num_tiles = int(hw_cfg['SYSTEM']['TILES'])
     rows, cols = util.factint(num_tiles)
     cellText = np.array([[0 for i in range(0,cols)]])
-    cellData = []
+    cellCycles = []
     maxLines = 0
     maxWidth = 0
     for r in range(0, rows):
         row = []
-        data = []
+        cycles = []
         for c in range(0, cols):
-            data.append(1)
             tile = r * cols + c + 1
             if (tile <= len(graph.nodes)):
                 node = graph.nodes[tile - 1]
-                string = str(tile) + "\n" + node.name.replace(";", "\n")
+                cycles.append(node.layer_cycles)
+                string = str(tile) + "\nCycles: {}".format(int(cycles[-1])) + "\n\n" + node.name.replace(";", "\n")
                 maxWidth = max(maxWidth, len(max(string.split("\n"), key= lambda s: len(s))))
                 maxLines = max(maxLines, len(string.split("\n")))
-                row.append(string)
+                if r % 2 == 1:
+                    row.insert(0, string)
+                else: row.append(string)
             else:
                 row.append("")
+                cycles.append(0)
         cellText = np.vstack([cellText, row])
-        cellData.append(data)
+        cellCycles.append(cycles)
     cellText = np.delete(cellText, 0, axis=0)
     print(maxWidth)
     print(maxLines)
@@ -39,8 +42,8 @@ def resource_table(hw_cfg, graph):
     cellHeight = 4 * maxLines
     sns.set(rc = {'figure.figsize':(cellWidth,cellHeight)})
     print(cellText)
-    ax = sns.heatmap(cellData, annot=cellText, fmt="s", cbar = False,  linewidths=.5)
-    plt.savefig("resource_table.svg")
+    ax = sns.heatmap(cellCycles, annot=cellText, fmt="s", cbar = True,  linewidths=.5)
+    plt.savefig("resource_heatmap.svg")
 
 
 
