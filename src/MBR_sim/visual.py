1c8e03bd6a18d72b637efb051c97c5af631c4282
MEHUL GOEL <mehulgoel873@gmail.com>
2022-10-06 17:32:42 -0700

    Fixed Resource Visual

--- src/MBR_sim/visual.py
+++ src/MBR_sim/visual.py
@@ -2,6 +2,8 @@
 from tkinter import font
 import util
 import matplotlib.pyplot as plt 
+import numpy as np; np.random.seed(0)
+import seaborn as sns
 
 # All kind of visulation
 
@@ -9,33 +11,36 @@ import matplotlib.pyplot as plt
 def resource_table(hw_cfg, graph):
     num_tiles = int(hw_cfg['SYSTEM']['TILES'])
     rows, cols = util.factint(num_tiles)
-    cellText = []
+    cellText = np.array([[0 for i in range(0,cols)]])
+    cellData = []
+    maxLines = 0
+    maxWidth = 0
     for r in range(0, rows):
         row = []
+        data = []
         for c in range(0, cols):
+            data.append(1)
             tile = r * cols + c + 1
             if (tile <= len(graph.nodes)):
                 node = graph.nodes[tile - 1]
-                row.append(str(tile) + "\n" + node.name.replace(";", "\n"))
+                string = str(tile) + "\n" + node.name.replace(";", "\n")
+                maxWidth = max(maxWidth, len(max(string.split("\n"), key= lambda s: len(s))))
+                maxLines = max(maxLines, len(string.split("\n")))
+                row.append(string)
             else:
                 row.append("")
-        cellText.append(row)
-    
-    plt.rcParams["figure.figsize"] = [100.00, 100.50]
-    plt.rcParams["figure.autolayout"] = True
-    fig, ax = plt.subplots()
-    ax.axis('tight')
-    ax.set_axis_off() 
-    table = ax.table( 
-        cellText = cellText,  
-        rowLabels = [i for i in range(0, rows)],  
-        colLabels = [i for i in range(0, cols)], 
-        cellLoc ='center',  
-        loc ='upper left')
-      
-    ax.set_title('Resources Mapped', 
-                fontweight ="bold")
-    plt.savefig("resource_table.svg", bbox_inches='tight')
+        cellText = np.vstack([cellText, row])
+        cellData.append(data)
+    cellText = np.delete(cellText, 0, axis=0)
+    print(maxWidth)
+    print(maxLines)
+
+    cellWidth = 1.25 * maxWidth
+    cellHeight = 4 * maxLines
+    sns.set(rc = {'figure.figsize':(cellWidth,cellHeight)})
+    print(cellText)
+    ax = sns.heatmap(cellData, annot=cellText, fmt="s", cbar = False,  linewidths=.5)
+    plt.savefig("resource_table.svg")
 
 
 
